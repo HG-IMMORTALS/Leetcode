11830d8b77a2ed5bcc4a0e0e6cdac14189103753
Hemant Gaur <95062216+HG-IMMORTALS@users.noreply.github.com>
2023-08-01 18:46:34 +0530

    Time: 21 ms (85.40%) | Memory: 18.7 MB (25.67%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-  vector<vector<int>> result;
+  vector<vector<int>>sol;
     
-    void solve(int start, int n, int k, vector<int>& temp) {
-        if(k == 0) {
-            result.push_back(temp);
+    void solve(int s,int n,int k,vector<int>& temp) {
+        if(k==0)
+				{
+            sol.push_back(temp);
             return;
         }
         
-        if(start > n)
+        if(s>n)
             return;
-        
-        temp.push_back(start);
-        solve(start+1, n, k-1, temp);
+        temp.push_back(s);
+        solve(s+1,n,k-1,temp);
         temp.pop_back();
-        solve(start+1, n, k, temp);
+        solve(s+1,n,k,temp);
     }
     
-    vector<vector<int>> combine(int n, int k) {
-        vector<int> temp;
-        
-        solve(1, n, k, temp);
-        
-        return result;
+    vector<vector<int>>combine(int n,int k) 
+		{
+        vector<int>temp;
+        solve(1,n,k,temp);
+        return sol;
 		}
 };
