54b318ea32c564529dbb1ffecaa3b3a7291c1519
Hemant Gaur <95062216+HG-IMMORTALS@users.noreply.github.com>
2023-08-24 08:01:44 +0530

    Time: 5 ms (16.99%) | Memory: 7.4 MB (39.27%) - LeetSync

---
+++ 68-text-justification/text-justification.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<string> fullJustify(vector<string>& words, int L) {
+    //         vector<string>sol;
+    // for(int i=0,k,l;i<words.size();i+=k)
+    // {
+    //     for(k=l=0;i+k<words.size() || l+words[i+k].size()<=L-k;k++)
+    //     {
+    //         l+=words[i+k].size();
+    //     }
+    //     string dusri=words[i];
+    //     for(int j=0;j<k-1;j++)
+    //     {
+    //         if(i+k>=words.size()) dusri+=" ";
+    //         else dusri+=string((L-l)/(k-1)+(j<(L-l)%(k-1)),' ');
+    //         dusri+=words[i+j+1];
+    //     }
+    //     dusri+=string(L-dusri.size(),' ');
+    //     sol.push_back(dusri);
+    // }
+    // return sol;
+    vector<string> res;
+    for(int i = 0, k, l; i < words.size(); i += k) {
+        for(k = l = 0; i + k < words.size() and l + words[i+k].size() <= L - k; k++) {
+            l += words[i+k].size();
+        }
+        string tmp = words[i];
+        for(int j = 0; j < k - 1; j++) {
+            if(i + k >= words.size()) tmp += " ";
+            else tmp += string((L - l) / (k - 1) + (j < (L - l) % (k - 1)), ' ');
+            tmp += words[i+j+1];
+        }
+        tmp += string(L - tmp.size(), ' ');
+        res.push_back(tmp);
+    }
+    return res;
+}
+};
